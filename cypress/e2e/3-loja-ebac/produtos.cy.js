056778d92ea1475ff8cfc9b1e4086b0b4cf9ea4d
joao pedro <joaomhass4@gmail.com>
2024-06-06 21:40:11 -0300

    Page Objects Model

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,29 @@
 /// <refence types="cypress" />
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('funcionalidade: produtos', () => {
 
     beforeEach (() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Ariel Roll Sleeve Sweatshirt')
-            .click()
-
+        produtosPage.buscarProdutoLista('Atlas Fitness Tank')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
+
+    it.only('deve buscar um produto com sucesso' , () => {
+        let produto = 'Apollo Running Short'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
+    });
+
+    it('deve visitar a pagina do produto' , () => {
+
+    });
+
+    it('deve adicionar produto ao carrinho' , () => {
+
+    });
+
 });
