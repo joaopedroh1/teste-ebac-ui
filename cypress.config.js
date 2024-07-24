7edf0acf9741de1a248fa8ce0fa3939508710097
joao pedro <joaomhass4@gmail.com>
2024-07-24 18:05:10 -0300

    modificação

--- cypress.config.js
+++ cypress.config.js
@@ -8,3 +8,11 @@ module.exports = defineConfig({
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
   },
 });
+
+cypress/e2e/spec.cy.js
+import user from '../fixtures/user.json'
+it('loads the same object', () => {
+  cy.fixture('user').then((userFixture) => {
+    expect(user, 'the same data').to.deep.equal(userFixture)
+  })
+})
--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -12,18 +12,36 @@ describe('funcionalidade: produtos', () => {
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
-    it.only('deve buscar um produto com sucesso' , () => {
+    it('deve buscar um produto com sucesso' , () => {
         let produto = 'Apollo Running Short'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain' , produto)
     });
 
     it('deve visitar a pagina do produto' , () => {
-
+        produtosPage.visitarProduto('Aero Daily Fitness Tee')
+        cy.get('.product_title').should('contain' , 'Aero Daily Fitness Tee')
     });
 
     it('deve adicionar produto ao carrinho' , () => {
+        let qtd = 9 
+        produtosPage.buscarProduto('Ingrid Running Jacket')
+        produtosPage.addProdutoCarrinho('M', 'Red', qtd)
 
+        cy.get('.woocommerce-message').should('contain',  qtd + ' × “Ingrid Running Jacket” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados' , () => {
+        cy.fixture('produtos ').then(dados => {
+            produtosPage.buscarProduto(dados[1].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[1].tamanho,
+                dados[1].cor,
+                dados[1].quantidade)
+
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+            
+        })
     });
 
 });
